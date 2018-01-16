11e0a9c5b118d594fed280e72360d8c3fdc48b6c
Jean Baptiste Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-01-17 00:14:26 +0100

    Ait ajouté dans le execue du Mojo, les traitements qui correspondraient à la copie du fichier war du poste du développeur, mais en Sftp cette fois. Une session interactive SFTP démarre et est effective, mais ce n'est pas ce que je veux comme résultat

--- deployeur/src/main/java/lasselle/deployeur/JiblMojo1.java
+++ deployeur/src/main/java/lasselle/deployeur/JiblMojo1.java
@@ -1,13 +1,18 @@
 package lasselle.deployeur;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+
+import lasselle.ssh.operations.elementaires.JiblExec;
+import lasselle.ssh.operations.elementaires.JiblScpTo;
 //import org.apache.maven.model.Model;
+import lasselle.ssh.operations.elementaires.JiblSftp;
 
 
 @Mojo( name = "deploie")
@@ -16,7 +21,8 @@ public class JiblMojo1 extends AbstractMojo {
 	/**
 	 * LEs paramètres du goal maven
 	 */
-
+	@Parameter(alias = "nom-conteneur-docker", property = "nom-conteneur-docker", required = true)
+	String nomConteneurDocker= null;
 	@Parameter(alias = "ip-cible", property = "ip-cible", required = true)
 	String adresseIPcibleDeploiement= null;
 	@Parameter(alias = "lx-user", property = "lx-user",defaultValue = "lauriane")
@@ -33,9 +39,9 @@ public class JiblMojo1 extends AbstractMojo {
 	@Parameter(defaultValue = "${project.build.directory}")
 	private String cheminProjet;
 	@Parameter(defaultValue = "${project.artifactId}-${project.version}.war")
-	private String cheminFichierWAR;
+	private String nomFichierWAR;
 	
-	private File fichierWAR = new File(cheminProjet + "/" + cheminFichierWAR);
+	private File fichierWAR = new File(cheminProjet + "/" + nomFichierWAR);
 	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
@@ -64,12 +70,38 @@ public class JiblMojo1 extends AbstractMojo {
 		System.out.println(" +++	VALEUR cheminProjet: " + this.cheminProjet + " ");
 		
 		System.out.println(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++ ");
-		System.out.println(" +++	VALEUR cheminFichierWAR: " + this.cheminFichierWAR + " ");
+		System.out.println(" +++	VALEUR cheminFichierWAR: " + this.nomFichierWAR + " ");
 		System.out.println(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++ ");
-		System.out.println(" +++	VALEUR cheminProjet + \"/\" + cheminFichierWAR: " + cheminProjet + "/" + cheminFichierWAR + " ");
+		System.out.println(" +++	VALEUR cheminProjet + \"/\" + cheminFichierWAR: " + cheminProjet + "/" + nomFichierWAR + " ");
 		System.out.println(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++ ");
 		System.out.println(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++ ");
-				
+		
+		
+		copierFihierWarVersCible();
+		System.out.println(" +++	COPIE FAITE DANS CONTENEUR ");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		executeLeDeploiement();
+		System.out.println(" +++	DEPLOIEMENT TERMINE DANS CONTENEUR ");
+		
 	}
+	
+	private void copierFihierWarVersCible() {
+//		JiblScpTo.faisCopie(this.cheminProjet + "/" + this.nomFichierWAR, this.nomFichierWAR, adresseIPcibleDeploiement, SSHusername, SSHuserpwd);
+		JiblSftp.faisCopie(this.cheminProjet + "/" + this.nomFichierWAR, this.nomFichierWAR, adresseIPcibleDeploiement, SSHusername, SSHuserpwd);
+		
+	}
+	private void executeLeDeploiement() {
+		
+		JiblExec.executeCetteCommande("sudo docker cp "+ this.nomFichierWAR + " " + this.nomConteneurDocker + ":/usr/local/tomcat/webapps", adresseIPcibleDeploiement, SSHusername, SSHuserpwd);
+		JiblExec.executeCetteCommande("sudo docker restart " + this.nomConteneurDocker, adresseIPcibleDeploiement, SSHusername, SSHuserpwd);
+		
+	}
+	
 
 }
--- deployeur/src/main/java/lasselle/ssh/operations/elementaires/JiblScpTo.java
+++ deployeur/src/main/java/lasselle/ssh/operations/elementaires/JiblScpTo.java
@@ -13,10 +13,37 @@ import java.io.*;
  * @author ezy
  *
  */
-public class ScpTo {
-	public static void main(String[] arg) {
+public class JiblScpTo {
+	
+	/**
+	 * effectue la copie que je souhaite faire
+	 * @param deCeFichier
+	 * @param AcetteAdresseIP
+	 * @param AvecCeUser
+	 * @param AvecCeMotdepasse
+	 */
+	public static void faisCopie(String deCeFichier, String nomDuFichierWarSeul, String AcetteAdresseIP, String AvecCeUser, String AvecCeMotdepasse) {
+		// String[] tableauArgs = {"nomfichierAcopier", "user@remotehost:nomfinaldufichierCopie"};
+		// même nom de fichier  en résultat de copie
+		String nomfinaldufichierCopie = nomDuFichierWarSeul;
+		String[] tableauArgs = {deCeFichier, AvecCeUser + "@"+AcetteAdresseIP+":" + nomfinaldufichierCopie};
+		JiblScpTo.Executer(tableauArgs, AvecCeMotdepasse);
+	}
+	/**
+	 * Anciennement: public static void main(String[] arg)
+	 * 
+	 * Donc, maintenant:
+	 * 
+	 * String[] tableauArgs = {"file1", "user@remotehost:file2"};
+	 * JiblScpTo.Executer(tableauArgs, "laur1@ne");
+	 * // tableauArgs = {"file1", "user@remotehost:file2"} Correspond à:
+	 * // 	# ligne de commande shell ou MS DOS
+	 * //	  [java ScpTo file1 user@remotehost:file2]
+	 * @param arg
+	 */
+	private static void Executer(String[] arg, String motdepasse) {
 		if (arg.length != 2) {
-			System.err.println("usage: java ScpTo file1 user@remotehost:file2");
+			System.err.println("usage: java ScpTo file1 user@remotehost:file2"); /// coorespond à appeler main avec le tableau // tableauArgs = {"ScpTo", "file1", "user@remotehost:file2"}
 			System.exit(-1);
 		}
 
@@ -33,7 +60,7 @@ public class ScpTo {
 			Session session = jsch.getSession(user, host, 22);
 
 			// username and password will be given via UserInfo interface.
-			UserInfo ui = new MyUserInfo();
+			UserInfo ui = new JiblUserInfo(motdepasse);
 			session.setUserInfo(ui);
 			session.connect();
 
@@ -145,16 +172,31 @@ public class ScpTo {
 		return b;
 	}
 
-	public static class MyUserInfo implements UserInfo, UIKeyboardInteractive {
+	/**
+	 * Tous les prompts doivent retrouner "true" pour que l'exécution se fasse sans interactivité à l'utilisateur.
+	 * Le mot de passe doit être fixé dans 
+	 * @author ezy
+	 *
+	 */
+	public static class JiblUserInfo implements UserInfo/*, UIKeyboardInteractive */{
+//		public JiblUserInfo() {
+//			super();
+//		}
+		public JiblUserInfo(String motdepasse) {
+			super();
+			this.passwd = motdepasse;
+		}
+		
 		public String getPassword() {
 			return passwd;
 		}
 
 		public boolean promptYesNo(String str) {
-			Object[] options = { "yes", "no" };
-			int foo = JOptionPane.showOptionDialog(null, str, "Warning", JOptionPane.DEFAULT_OPTION,
-					JOptionPane.WARNING_MESSAGE, null, options, options[0]);
-			return foo == 0;
+//			Object[] options = { "yes", "no" };
+//			int foo = JOptionPane.showOptionDialog(null, str, "Warning", JOptionPane.DEFAULT_OPTION,
+//					JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+//			return foo == 0;
+			return true;
 		}
 
 		String passwd;
@@ -169,66 +211,23 @@ public class ScpTo {
 		}
 
 		public boolean promptPassword(String message) {
-			Object[] ob = { passwordField };
-			int result = JOptionPane.showConfirmDialog(null, ob, message, JOptionPane.OK_CANCEL_OPTION);
-			if (result == JOptionPane.OK_OPTION) {
-				passwd = passwordField.getText();
-				return true;
-			} else {
-				return false;
-			}
+//			Object[] ob = { passwordField };
+//			int result = JOptionPane.showConfirmDialog(null, ob, message, JOptionPane.OK_CANCEL_OPTION);
+//			if (result == JOptionPane.OK_OPTION) {
+//				passwd = passwordField.getText();
+//				return true;
+//			} else {
+//				return false;
+//			}
+			return true;
 		}
 
 		public void showMessage(String message) {
-			JOptionPane.showMessageDialog(null, message);
+//			JOptionPane.showMessageDialog(null, message);
 		}
 
-		final GridBagConstraints gbc = new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
-		private Container panel;
 
-		public String[] promptKeyboardInteractive(String destination, String name, String instruction, String[] prompt,
-				boolean[] echo) {
-			panel = new JPanel();
-			panel.setLayout(new GridBagLayout());
 
-			gbc.weightx = 1.0;
-			gbc.gridwidth = GridBagConstraints.REMAINDER;
-			gbc.gridx = 0;
-			panel.add(new JLabel(instruction), gbc);
-			gbc.gridy++;
 
-			gbc.gridwidth = GridBagConstraints.RELATIVE;
-
-			JTextField[] texts = new JTextField[prompt.length];
-			for (int i = 0; i < prompt.length; i++) {
-				gbc.fill = GridBagConstraints.NONE;
-				gbc.gridx = 0;
-				gbc.weightx = 1;
-				panel.add(new JLabel(prompt[i]), gbc);
-
-				gbc.gridx = 1;
-				gbc.fill = GridBagConstraints.HORIZONTAL;
-				gbc.weighty = 1;
-				if (echo[i]) {
-					texts[i] = new JTextField(20);
-				} else {
-					texts[i] = new JPasswordField(20);
-				}
-				panel.add(texts[i], gbc);
-				gbc.gridy++;
-			}
-
-			if (JOptionPane.showConfirmDialog(null, panel, destination + ": " + name, JOptionPane.OK_CANCEL_OPTION,
-					JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION) {
-				String[] response = new String[prompt.length];
-				for (int i = 0; i < prompt.length; i++) {
-					response[i] = texts[i].getText();
-				}
-				return response;
-			} else {
-				return null; // cancel
-			}
-		}
 	}
 }
